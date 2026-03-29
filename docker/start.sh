159ac4d7857a539aa270a28d60850792e8aee23a
Juan Pedrajas Mendoza <juan.pedrajas@ebury.com>
2026-03-29 16:45:40 +0200

    fix: create enviroment to run tests properly

--- docker/start.sh
+++ docker/start.sh
@@ -2,6 +2,7 @@
 
 # Start the backend (Uvicorn)
 cd /app/backend
+poetry lock
 poetry run uvicorn main:app --host 0.0.0.0 --port 8000 &
 BACKEND_PID=$!
 echo "Backend started with PID: $BACKEND_PID"
