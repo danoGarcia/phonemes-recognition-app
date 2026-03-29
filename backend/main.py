f89325c2944c62eeecc651d6ce9ed0a6135698ce
Juan Pedrajas Mendoza <juan.pedrajas@ebury.com>
2026-03-29 16:46:06 +0200

    feat: implement dictionary service

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,12 @@
 from fastapi import FastAPI
 
+from app.api.v1.dictionary import router as dictionary_router
+
 app = FastAPI()
+
+app.include_router(dictionary_router, prefix="/api/v1")
 
 
 @app.get("/")
-def read_root():
+async def read_root():
     return {"message": "Hello, world!"}
